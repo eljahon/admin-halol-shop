cdabb552263a30fd22d6402f851d225b98afaf7a
eljahon <neljahon@gmail.com>
2024-09-05 13:29:34 +0500

    farmes add crdue

--- src/components/Forms/FormBuilder.vue
+++ src/components/Forms/FormBuilder.vue
@@ -41,15 +41,29 @@ const props = defineProps({
         type: Object,
         required: false,
         default:() => {}
+    },
+    methods:{
+        type: Array,
+        default: () => []
     }
 });
 const emit = defineEmits(['handelSubmitForm']);
+const phone = yup.string()
+    .matches(/^\+998\d{9}$/, 'Telefon raqami +998 bilan boshlanib, 9 ta raqam bo\'lishi kerak')
+    .required('Telefon raqami majburiy')
+const vlist ={
+    'string': yup.string(),
+    'number': yup.number(),
+    'date': yup.date(),
+    'phone': phone
+}
+const validationList = (name) => vlist[name]
 const route = useRoute();
 const id = route.params.id;
 const { handleSubmit, defineField, resetForm, errors, setFieldValue, values, isSubmitting } = useForm({
     validationSchema: yup.object(
         props.feilds.reduce((acc, el) => {
-            acc[el.label] = el.schema.required ? yup[el.schema.type]().required(t(`${el.label}`) + ' ' + t('required-fild')) : '';
+            acc[el.label] = el.schema.required ? validationList(el.schema.type).required(t(`${el.label}`) + ' ' + t('required-fild')) : '';
             return acc;
         }, {})
     ),
@@ -94,7 +108,7 @@ watch(() => updateValue.value,
                 <div v-else>
                     <div :class="[item.prop.class]">
                         <label class="block my-1">{{ $t(`${item.label}`) }}</label>
-                        <component :invalid="!!errors[item.label]" class="w-full" v-bind="item.prop" :is="componentList[item.renderElement]" v-model="defineFieldForm[index][0].value" :type="item.prop?.type" :placeholder="$t(`${item.label}`)">
+                        <component :invalid="!!errors[item.label]" class="w-full" v-bind="item.prop" :is="componentList[item.renderElement]" v-model="defineFieldForm[index][0].value" @update:modelValue="methods[index]" :type="item.prop?.type" :placeholder="$t(`${item.label}`)">
                         </component>
                     </div>
                     <small class="text-red-500 block">
--- src/views/pages/users/farmers/farmers-list.vue
+++ src/views/pages/users/farmers/farmers-list.vue
@@ -6,12 +6,8 @@ import TheBreadcrumb from '@/components/The-Breadcrumb.vue';
 import { useStore } from 'vuex';
 import { actions } from '@/utils/Store_Schema';
 import { useRoute, useRouter } from 'vue-router';
-import dayjs from 'dayjs';
 import PaginatorCustom from '@/components/Paginator-Custom.vue';
-import ImageOnLoad from '@/components/ImageOnLoad.vue';
-import Exceljs from 'exceljs';
 import { useI18n } from 'vue-i18n';
-import { debounce } from 'lodash';
 const store = useStore();
 const router = useRouter();
 const route = useRoute();
@@ -27,16 +23,19 @@ let list = {
     true: { name: t('is_main'), id: true },
     false: { name: t('is_main_not'), id: false }
 };
-const cropsFilter = ref(list[route.query?.is_common].id);
-const search = ref(route.query?.search ?? '');
+const regions = ref(undefined);
+const region = ref(route.query.region ? +route.query.region : undefined);
+const districts = ref(undefined);
+const district = ref(route.query.district ? +route.query.district : undefined);
+const areas = ref();
+const area = ref(route.query.area ? +route.query.area : undefined);
 const meta = ref({});
 
-
-const { getCrops, deleteCrops,getCropAdmin} = actions(['crops', 'farmers'], { get: true, remove: true });
+const { getFarmers, deleteFarmers, getRegions, getDistricts, getAreas } = actions(['farmers', 'regions', 'districts', 'areas'], { get: true, remove: true });
+// console.log(action, 'list');
 function openNew() {
-    router.push({ name: 'crops-create', params: { id: 'new' } });
+    router.push({ name: 'farmers-create', params: { id: 'new' } });
 }
-
 
 function confirmDeleteProduct(prod) {
     crop.value = prod;
@@ -46,12 +45,12 @@ function confirmDeleteProduct(prod) {
 function deleteProduct() {
     deleteCropDialog.value = false;
     isLoading.value = true;
-    deleteCrops(crop.value.id)
+    deleteFarmers(crop.value.id)
         .then((res) => {
             crop.value = {};
-            toast.add({ severity: 'success', summary: 'Successful', detail: t('crops')+' '+t('delete'), life: 3000 });
+            toast.add({ severity: 'success', summary: 'Successful', detail: t('crops') + ' ' + t('delete'), life: 3000 });
             isLoading.value = false;
-            getCropsList()
+            getCropsList();
         })
         .catch((err) => {
             toast.add({ severity: 'error', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
@@ -59,34 +58,8 @@ function deleteProduct() {
         });
 }
 
-const excelDownload = async () => {
-    const workbook = new Exceljs.Workbook();
-    const worksheet = workbook.addWorksheet('Data');
-    worksheet.addRow(['Id', 'Name', 'Biology name', 'Description', 'Details', 'Diseases', 'Season', 'Drugs', 'Fertilizers']);
-    for (let i = 0; i < crops.value.length; i++) {
-        worksheet.columns = [{ width: 5 }, { width: 16 }, { width: 16 }, { width: 48 }, { width: 48 }, { width: 16 }, { width: 16 }, { width: 16 }, { width: 16 }];
-        const { id, name, biology_name, description, details, diseases, season, drugs, fertilizers } = crops.value[i];
-        worksheet.addRow([id, name, biology_name, description, details, diseases?.name, season?.title, drugs?.name, fertilizers?.name]);
-    }
-    const buffer = await workbook.xlsx.writeBuffer();
-    const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'crops.xlsx');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
-function getStatusLabel(status) {
-    if (status) return 'success';
-
-    return 'danger';
-}
 async function routerPush(param) {
-    console.log(route.query, '==>>', param);
-    const _query ={...route.query, ...param}
-    console.log(_query);
+    const _query = { ...route.query, ...param };
     await router.replace({ query: _query });
     await getCropsList();
 }
@@ -95,9 +68,9 @@ function onChangeSelect(value) {
     routerPush({ is_common: value, page: 1 });
 }
 
-let onSearch = debounce(function(value) {
-    routerPush({ search: value.target?.value ? value.target?.value : undefined, page: 1 });
-}, 1500)
+// let onSearch = debounce(function(value) {
+//     routerPush({ search: value.target?.value ? value.target?.value : undefined, page: 1 });
+// }, 1500)
 function getCropsList() {
     isLoading.value = true;
     const _query = { ...route.query };
@@ -105,18 +78,16 @@ function getCropsList() {
         populate: '*',
         sort: 'createdAt:desc',
         pagination: { page: _query?.page ? +_query?.page : 1, pageSize: _query.pageSize ? +_query.pageSize : 25 },
-        filters: {
-            is_common: _query?.is_common ?? undefined,
-            name: {
-                $containsi: _query.search ?? undefined
-            }
-        }
+        region: _query.region ?? undefined,
+        district: _query.district ?? undefined,
+        area: _query.area ?? undefined
     };
-    getCropAdmin(filters)
+    return getFarmers(filters)
         .then((res) => {
             crops.value = res.data;
             meta.value = res.meta;
             isLoading.value = false;
+            return res;
         })
         .catch((err) => {
             isLoading.value = false;
@@ -125,17 +96,57 @@ function getCropsList() {
     //     isLoading.value =false
     // });
 }
+async function getAreasList() {
+    const _query = { ...route.query };
+    return await getAreas({populate: '*', district:_query.district ?? undefined}).then((res) => {
+        areas.value = res.data;
+    });
+}
+async function getDistrictList() {
+    const _query = { ...route.query };
+    return await getDistricts({ populate: '*', filters: { region: { id: _query.region ?? undefined } } }).then((res) => {
+        districts.value = res.data;
+    });
+}
+async function getRegionsList() {
+    const _query = { ...route.query };
 
+    return await getRegions({ populate: '*' }).then((res) => {
+        regions.value = res.data;
+    });
+}
 getCropsList();
-
+getRegionsList();
+getDistrictList();
+getAreasList();
+async function filterAreas(value) {
+    if(value) {
+        await routerPush({ area: value });
+    } else {
+        await routerPush({ area: undefined });
+    }
+}
+async function filterDistrict(value) {
+    if (value) {
+        await routerPush({ district: value });
+        await getAreas();
+    } else {
+        await routerPush({ district: undefined });
+        await getAreas();
+    }
+}
+async function filterRegion(value) {
+    if (value) {
+        await routerPush({ region: value });
+        await getDistricts();
+    } else {
+        await routerPush({ region: undefined });
+        await getDistricts();
+    }
+}
 function onChangePage(value) {
     getCropsList();
 }
-
-// onSearch = debounce(onSearch, 1500);
-watch(() => route.query, (value) => {
-    console.log(value, 'valtcha');
-})
 </script>
 
 <template>
@@ -145,77 +156,71 @@ watch(() => route.query, (value) => {
             <div class="flex gap-2 justify-between">
                 <div>
                     <div class="flex flex-wrap gap-2 items-center justify-between">
-                        <Select
-                            class="w-full md:w-56"
-                            v-model="cropsFilter"
-                            :placeholder="$t('all')"
-                            optionLabel="name"
-                            optionValue="id"
-                            clearable
-                            @update:modelValue="onChangeSelect"
-                            :options="[
-                                { name: $t('all'), id: undefined },
-                                { name: $t('is_main'), id: true },
-                                { name: $t('is_main_not'), id: false }
-                            ]"
-                        >
-                        </Select>
-                        <IconField>
-                            <InputIcon>
-                                <i class="pi pi-search" />
-                            </InputIcon>
-                            <InputText v-model="search" :placeholder="$t('search-by-name')" @input="onSearch" />
-                        </IconField>
+                        <Select class="w-full md:w-56" v-model="region" :placeholder="$t('region')" optionLabel="name" optionValue="id" clearable showClear @update:modelValue="filterRegion" :options="regions"> </Select>
+                        <Select class="w-full md:w-56" v-model="district" :placeholder="$t('district')" optionLabel="name" optionValue="id" clearable showClear @update:modelValue="filterDistrict" :options="districts"> </Select>
+                        <Select class="w-full md:w-56" v-model="area" :placeholder="$t('area')" optionLabel="name" optionValue="id" clearable showClear @update:modelValue="filterAreas" :options="areas"> </Select>
+                        <!--                        <IconField>-->
+                        <!--                            <InputIcon>-->
+                        <!--                                <i class="pi pi-search" />-->
+                        <!--                            </InputIcon>-->
+                        <!--                            <InputText v-model="search" :placeholder="$t('search-by-name')" @input="onSearch" />-->
+                        <!--                        </IconField>-->
                     </div>
                 </div>
                 <div class="flex gap-2">
-                    <Button label="Export" icon="pi pi-upload" severity="success" @click="excelDownload" />
+                    <!--                    <Button label="Export" icon="pi pi-upload" severity="success" @click="excelDownload" />-->
                     <Button label="New" icon="pi pi-plus" severity="success" class="mr-2" @click="openNew" />
                 </div>
             </div>
             <DataTable ref="dt" :value="crops" dataKey="id" :loading="isLoading">
                 <template #header> </template>
                 <Column field="id" :header="$t('id')">
-                    <template #loading>
-                        <div class="flex items-center" :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }">
-                            <Skeleton width="60%" height="1rem" />
-                        </div>
-                    </template>
+                    <!--                    <template #loading>-->
+                    <!--                        <div class="flex items-center" :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }">-->
+                    <!--                            <Skeleton width="60%" height="1rem" />-->
+                    <!--                        </div>-->
+                    <!--                    </template>-->
                 </Column>
-                <Column :header="$t('image')" style="min-width: 8rem">
+                <Column :header="$t('region-district-area')" style="min-width: 16rem">
                     <template #body="{ data }">
-                        <ImageOnLoad width="90" :src="data?.main_image?.aws_path" />
+                        <!--                        {{data}}-->
+                        <span class="text-primary">{{ data?.region?.name }}</span
+                        >, <span class="text-primary">{{ data?.district?.name }}</span
+                        >, <span class="text-primary">{{ data?.area?.name }}</span>
+                        <!--                        <ImageOnLoad width="90" :src="data?.main_image?.aws_path" />-->
                     </template>
                 </Column>
-                <Column field="name" :header="$t('name')" style="min-width: 16rem"></Column>
+                <Column field="farmer_name" :header="$t('farmer-name')" style="min-width: 8rem"></Column>
+                <Column field="fullname" :header="$t('farmer-manger')" style="min-width: 8rem"></Column>
+                <Column field="phone" :header="$t('phone')" style="min-width: 10rem"></Column>
 
-                <Column field="price" :header="$t('biology_name')" style="min-width: 9rem">
+                <Column field="field_area" :header="$t('hectare')" style="min-width: 9rem">
                     <template #body="{ data }">
-                        {{ data?.biology_name ?? '-' }}
+                        <Tag>{{ (data?.field_area / 10000).toFixed(2) ?? '-' }} {{ $t('hectare') }}</Tag>
                     </template>
                 </Column>
-                <Column field="category" :header="$t('planting_time_start')" style="min-width: 12rem; text-align: center">
-                    <template #body="{ data }">
-                        <span v-if="data.planting_time_start">{{ dayjs(data.planting_time_start).format('DD-MM-YYYY') }}</span>
-                        <span v-else class="text-red-500">{{ '--' }}</span>
-                    </template>
-                </Column>
-                <Column field="rating" :header="$t('planting_time_end')" style="min-width: 12rem; text-align: center">
-                    <template #body="{ data }">
-                        <span v-if="data.planting_time_end">{{ dayjs(data.planting_time_end).format('DD-MM-YYYY') }}</span>
-                        <span v-else class="text-red-500">{{ '--' }}</span>
-                        <!--                        <Rating :modelValue="slotProps.data.rating" :readonly="true" />-->
-                    </template>
-                </Column>
-                <Column field="inventoryStatus" :header="$t('active')" style="min-width: 12rem">
-                    <template #body="slotProps">
-                        <Tag :value="slotProps.data.is_common ? $t('is_main') : $t('is_main_not')" :severity="getStatusLabel(slotProps.data.is_common)" />
-                    </template>
-                </Column>
+                <!--                <Column field="category" :header="$t('planting_time_start')" style="min-width: 12rem; text-align: center">-->
+                <!--                    <template #body="{ data }">-->
+                <!--                        <span v-if="data.planting_time_start">{{ dayjs(data.planting_time_start).format('DD-MM-YYYY') }}</span>-->
+                <!--                        <span v-else class="text-red-500">{{ '&#45;&#45;' }}</span>-->
+                <!--                    </template>-->
+                <!--                </Column>-->
+                <!--                <Column field="rating" :header="$t('planting_time_end')" style="min-width: 12rem; text-align: center">-->
+                <!--                    <template #body="{ data }">-->
+                <!--                        <span v-if="data.planting_time_end">{{ dayjs(data.planting_time_end).format('DD-MM-YYYY') }}</span>-->
+                <!--                        <span v-else class="text-red-500">{{ '&#45;&#45;' }}</span>-->
+                <!--                        &lt;!&ndash;                        <Rating :modelValue="slotProps.data.rating" :readonly="true" />&ndash;&gt;-->
+                <!--                    </template>-->
+                <!--                </Column>-->
+                <!--                <Column field="inventoryStatus" :header="$t('active')" style="min-width: 12rem">-->
+                <!--                    <template #body="slotProps">-->
+                <!--                        <Tag :value="slotProps.data.is_common ? $t('is_main') : $t('is_main_not')" :severity="getStatusLabel(slotProps.data.is_common)" />-->
+                <!--                    </template>-->
+                <!--                </Column>-->
                 <Column :header="$t('actions')" :frozen="actions" align-frozen="left" style="min-width: 12rem">
                     <template #body="{ data }">
-                        <Button icon="pi pi-eye" outlined rounded severity="info" class="mr-2" @click="router.push({ name: 'crops-info', query: { id: data.id } })" />
-                        <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="router.push({ name: 'crops-create', params: { id: data.id } })" />
+                        <Button icon="pi pi-eye" outlined rounded severity="info" class="mr-2" @click="router.push({ name: 'farmers-info', query: { id: data.id } })" />
+                        <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="router.push({ name: 'farmers-create', params: { id: data.id } })" />
                         <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(data)" />
                     </template>
                 </Column>
@@ -231,8 +236,8 @@ watch(() => route.query, (value) => {
             <div class="flex items-center gap-4">
                 <i class="pi pi-exclamation-triangle !text-3xl" />
                 <span v-if="crop"
-                >{{ $t('you-want-to-delete') }} <b>{{ crop.name }}</b
-                >?</span
+                    >{{ $t('you-want-to-delete') }} <b>{{ crop.name }}</b
+                    >?</span
                 >
             </div>
             <template #footer>
